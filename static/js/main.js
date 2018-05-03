f3be045414699fcdc0aebb20286428dae7116fcf
Andres Segundo <aisegund@ucsc.edu>
2018-05-03 11:50:54 -0700

    merged main.js incorrectly last time

--- static/js/main.js
+++ static/js/main.js
@@ -3,13 +3,12 @@ function openNav() {
 		document.getElementById("sidebar-container").style.visibility = "visible";
 	}, 200);
     document.getElementById("sidebar").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     document.getElementById("main").style.opacity = "0.5";
 	document.getElementById("logo_container").style.marginLeft = "180px";
-}
+ }
 
-
-function closeNav() {
+ function closeNav() {
     document.getElementById("sidebar-container").style.visibility = "hidden";
     document.getElementById("sidebar").style.width = "0";
     document.body.style.backgroundColor = "lightgray";
